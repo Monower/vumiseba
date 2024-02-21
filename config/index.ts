46816c4b5580a3db67f2acb35decbd8256c1d9bc
monower <sadafahmed720@gmail.com>
2024-02-21 22:07:41 +0600

    minor fix

---
+++ config/index.ts
@@ -0,0 +1,10 @@
+export const API_URL =  process.env.NEXT_PUBLIC_API_URL || 'https://devapi.vumiseba.com.bd'
+export const ADMIN_PANEL_URL = 'https://devadmin.vumiseba.com.bd'
+
+
+
+
+
+
+export const PER_PAGE = 3
+export const Review_Limit = 8
---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const webpack = require('webpack');
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'devapi.vumiseba.com.bd',
+      },
+    ],
+  },
+  reactStrictMode: false,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+      })
+    );
+    return config;
+  },
+};
+
+module.exports = nextConfig;
